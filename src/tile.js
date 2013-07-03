ce97d0de219e560ff14a80cdf2886a8cdfb3171f
Metallix <metallixcreations@gmail.com>
2013-07-03 18:53:52 +0200

    endless endlessness scrolling;

--- src/tile.js
+++ src/tile.js
@@ -21,13 +21,15 @@ var Layer = function( tileString ) {
 Layer.prototype = new createjs.Container();
 Layer.prototype.constructor = Layer;
 
-Layer.prototype.initialize = function() {
+Layer.prototype.base_initialize = Layer.prototype.initialize;
+Layer.prototype.initialize = function () {
+    this.base_initialize();
     this.chunks.push( new LayerChunk( this.tileString.substring( 0, 5 ) ) );
-    this.chunks.push( new LayerChunk( this.tileString.substring( 5, 5 ) ) );
-    this.chunks.push( new LayerChunk( this.tileString.substring( 10, 5 ) ) );
+    this.chunks.push( new LayerChunk( this.tileString.substring( 5, 10 ) ) );
+    this.chunks.push( new LayerChunk( this.tileString.substring( 10, 15 ) ) );
     for ( var i = 0; i < this.chunks.length; i++ ) {
         this.addChild( this.chunks[i] );
-        this.chunks[i].x = i * 5 * TILE_WIDTH;
+        this.chunks[i].x = i * 5 * TILE_WIDTH - 5 * TILE_WIDTH;
     }
 };
 
@@ -35,6 +37,20 @@ Layer.prototype.initialize = function() {
 Layer.prototype.moveByOffset = function(offset)
 {
     this.x += offset;
+    if ( this.x > 5 * TILE_WIDTH ) {
+        this.x -= 5 * TILE_WIDTH;
+        this.chunks.unshift( this.chunks.pop() );
+        for ( var i = 0; i < this.chunks.length; i++ ) {
+            this.chunks[i].x = i * 5 * TILE_WIDTH - 5 * TILE_WIDTH;
+        }
+    }
+    else if ( this.x < -5 * TILE_WIDTH ) {
+        this.x += 5 * TILE_WIDTH;
+        this.chunks.push( this.chunks.shift() );
+        for ( var i = 0; i < this.chunks.length; i++ ) {
+            this.chunks[i].x = i * 5 * TILE_WIDTH - 5 * TILE_WIDTH;
+        }
+    }
 };
 
 var LayerChunk = function ( tileString ) {
@@ -45,12 +61,14 @@ var LayerChunk = function ( tileString ) {
 LayerChunk.prototype = new createjs.Shape();
 LayerChunk.prototype.constructor = LayerChunk;
 
+LayerChunk.prototype.base_initialize = LayerChunk.prototype.initialize;
 LayerChunk.prototype.initialize = function () {
+    this.base_initialize();
     this.graphics.c();
     for ( var i = 0; i < this.tileString.length; i++ ) {
         var images = TILELIB[this.tileString.charAt( i ) ].images;
         var image = assetLoader.getResult( images[ Math.floor( Math.random() * images.length ) ] );
-        var bgimage = assetLoader.getResult( "tile_bg" + Math.floor( Math.random() * 5 ) );
+        var bgimage = assetLoader.getResult( "tile_bg" + Math.floor( Math.random() * 6 ) );
         this.graphics
             .bf( new Tile( bgimage ).image )
             .drawRect( TILE_WIDTH * i, 0, TILE_WIDTH, TILE_HEIGHT )
@@ -58,10 +76,6 @@ LayerChunk.prototype.initialize = function () {
             .drawRect( TILE_WIDTH * i, 0, TILE_WIDTH, TILE_HEIGHT );
     }
     this.cache( 0, 0, TILE_WIDTH * this.tileString.length, TILE_HEIGHT );
-};
-
-LayerChunk.prototype.moveByOffset = function ( offset ) {
-    this.x += offset;
 };
 
 var Level = function()
@@ -81,7 +95,7 @@ Level.prototype.initialize = function()
 {
     for(var i = 0; i < 6; ++i)
     {
-        var layer = new LayerChunk("W__W______WWWWW");
+        var layer = new Layer("W__W______WWWWW");
         layer.y = TILE_HEIGHT*i;
         this.addChild( layer );
         this.layers_[i] = layer;
@@ -95,7 +109,7 @@ Level.prototype.moveLayer = function(layerNo, offset)
 
 Level.prototype.translateWorld = function(x, y)
 {
-    // this.y = y % TILE_WIDTH;
+    //this.y = y % TILE_WIDTH;
 };
 
 Level.prototype.getLayerForPoint = function ( x, y )
