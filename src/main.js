6f7631e45abdf4b56f91c69d09ccf1eda58a3a2b
Metallix <metallixcreations@gmail.com>
2013-07-03 10:24:49 +0200

    easeljs example

---
+++ src/main.js
@@ -0,0 +1,32 @@
+﻿function startMainLoop() {
+    var stage = new createjs.Stage( "canvas" );
+
+    createjs.Ticker.addEventListener( "tick", update );
+    createjs.Ticker.useRAF = true;
+    createjs.Ticker.setFPS( 60 );
+
+    // REMOVE ME: Test graphic
+    var circle = new createjs.Shape();
+    circle.graphics.beginFill( "red" ).drawCircle( 0, 0, 50 );
+    circle.x = 100;
+    circle.y = 100;
+    stage.addChild( circle );
+
+    // REMOVE ME: Touch controls
+    createjs.Touch.enable( stage );
+
+    stage.addEventListener( "stagemousedown", touchStart );
+    stage.addEventListener( "stagemouseup", touchEnd );
+
+    function update() {
+        stage.update();
+    }
+
+    function touchStart( evt ) {
+        createjs.Tween.get( circle, { override : true } ).to( { scaleX: 2 }, 200 );
+    }
+
+    function touchEnd( evt ) {
+        createjs.Tween.get( circle, { override: true } ).to( { scaleX: 1 }, 200 );
+    }
+}
